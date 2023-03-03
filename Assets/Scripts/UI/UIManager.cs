050d098d3e6a343f1dc0a2f83838628a24bea912
Nick Perrin <nick.perrin@hotmail.com>
2023-03-03 00:46:29 -0500

    Remove debug code

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -6,28 +6,5 @@ namespace UI
     {
         [SerializeField]
         private FadeOverlay fadeOverlay;
-    
-        private void Update()
-        {
-            if (Input.GetKeyDown(KeyCode.Alpha1))
-            {
-                fadeOverlay.Fade(Color.black, 1, 1);
-            }
-        
-            if (Input.GetKeyDown(KeyCode.Alpha2))
-            {
-                fadeOverlay.Fade(Color.black, 0, 1);
-            }
-        
-            if (Input.GetKeyDown(KeyCode.Alpha3))
-            {
-                fadeOverlay.Fade(Color.green, .5f, .25f);
-            }
-        
-            if (Input.GetKeyDown(KeyCode.Alpha4))
-            {
-                fadeOverlay.Fade(Color.magenta, .75f, 4);
-            }
-        }
     }
 }
